5592794c42bd377e1500573acc40ace16354a23e
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-05-26 22:54:56 +0600

    handled deployment error

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -4,28 +4,28 @@ import SocialLogin from "../../../components/auth/SocialLogin";
 
 const LoginPage = () => {
   return (
-    <div class="contain py-16">
-      <div class="max-w-lg mx-auto shadow px-6 py-7 rounded overflow-hidden">
-        <h2 class="text-2xl uppercase font-medium mb-1">Login</h2>
-        <p class="text-gray-600 mb-6 text-sm">Welcome Back</p>
+    <div className="contain py-16">
+      <div className="max-w-lg mx-auto shadow px-6 py-7 rounded overflow-hidden">
+        <h2 className="text-2xl uppercase font-medium mb-1">Login</h2>
+        <p className="text-gray-600 mb-6 text-sm">Welcome Back</p>
 
         {/* Login Form */}
 
         <LoginForm />
 
-        <div class="mt-6 flex justify-center relative">
-          <div class="text-gray-600 uppercase px-3 bg-white z-10 relative">
+        <div className="mt-6 flex justify-center relative">
+          <div className="text-gray-600 uppercase px-3 bg-white z-10 relative">
             Or login with
           </div>
-          <div class="absolute left-0 top-3 w-full border-b-2 border-gray-200"></div>
+          <div className="absolute left-0 top-3 w-full border-b-2 border-gray-200"></div>
         </div>
 
         {/* Login with social accounts*/}
         <SocialLogin />
 
-        <p class="mt-4 text-center text-gray-600">
-          Don't have account?{" "}
-          <Link href="/register" class="text-primary">
+        <p className="mt-4 text-center text-gray-600">
+          Don&apos;t have account?{" "}
+          <Link href="/register" className="text-primary">
             Register now
           </Link>
         </p>
